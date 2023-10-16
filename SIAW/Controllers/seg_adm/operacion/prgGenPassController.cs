4c079536645a5ad8528c54c0bf463e9d1836a9ef
Rodrigo312-dev <rodrigomachacajimenez312@gmail.com>
2023-10-16 18:42:50 -0400

    contraseña especial

---
+++ SIAW/Controllers/seg_adm/operacion/prgGenPassController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using siaw_DBContext.Data;
+using siaw_DBContext.Models;
+using siaw_DBContext.Models_Extra;
+using siaw_funciones;
+using System.Net;
+
+namespace SIAW.Controllers.seg_adm.operacion
+{
+    [Route("api/seg_adm/oper/[controller]")]
+    [ApiController]
+    public class prgGenPassController : ControllerBase
+    {
+        private readonly UserConnectionManager _userConnectionManager;
+        private readonly Funciones funciones = new Funciones();
+        public prgGenPassController(UserConnectionManager userConnectionManager)
+        {
+            _userConnectionManager = userConnectionManager;
+        }
+
+        // GET: api/semodulo
+        [HttpGet]
+        [Route("genAutEspecial/{codalmacen}/{dato_a}/{dato_b}/{servicio}")]
+        public async Task<ActionResult<object>> genAutEspecial(int codalmacen, string dato_a, string dato_b, string servicio)
+        {
+            try
+            {
+                DateTime fechaAct = DateTime.Now;
+                int hora = fechaAct.Hour;
+                string pass = await funciones.SP(fechaAct, hora, codalmacen, dato_a, dato_b, servicio);
+                return Ok(pass);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Error en el servidor");
+            }
+
+
+        }
+
+    }
+}
---
+++ siaw_funciones/Funciones.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using siaw_DBContext.Data;
+
+namespace siaw_funciones
+{
+    public class Funciones
+    {
+        //Clase necesaria para el uso del DBContext del proyecto siaw_Context
+        public static class DbContextFactory
+        {
+            public static DBContext Create(string connectionString)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<DBContext>();
+                optionsBuilder.UseSqlServer(connectionString);
+
+                return new DBContext(optionsBuilder.Options);
+            }
+        }
+
+        public async Task<string> SP(DateTime fecha, int hora, int codalmacen, string dato_a, string dato_b, string servicio)
+        {
+            string resultado = "";
+            string cadena = "";
+            cadena = dato_a + hora.ToString() + dato_b + fecha.Year.ToString("0000") + servicio + fecha.Month.ToString("00") + codalmacen.ToString() + fecha.Day.ToString("00");
+            resultado = await encriptarMD5llave_yea(cadena);
+            if (resultado.Length >= 8)
+            {
+                resultado = resultado.Substring(resultado.Length - 7, 6);
+            }
+            return resultado.ToUpper();
+        }
+
+
+        public async Task<string> encriptarMD5llave_yea(string ClearString)
+        {
+            ClearString = "slip" + ClearString + "knot";
+            System.Text.UnicodeEncoding uEncode = new System.Text.UnicodeEncoding();
+            byte[] bytClearString = uEncode.GetBytes(ClearString);
+            System.Security.Cryptography.MD5CryptoServiceProvider sha = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] hash = sha.ComputeHash(bytClearString);
+            return Convert.ToBase64String(hash).Substring(1, 8);
+        }
+    }
+}
